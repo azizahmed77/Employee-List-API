0d9a4a6ef1f4fe70e30fd9ddcc76c5c06483a9fd
aziz <ahmed613@umn.edu>
2022-11-11 12:37:26 -0600

    initial

---
+++ angular-employeelist/FullstackAPI/EmployeelistAPI/Data/EmployeeListContext.cs
@@ -0,0 +1,14 @@
+﻿using EmployeelistAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeelistAPI.Data
+{
+    public class EmployeeListContext : DbContext
+    {
+        public EmployeeListContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
